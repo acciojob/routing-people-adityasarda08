1d0624b4249fec2b3d55dbdf3920d9454ca6d9e2
aditya <adityasarda08.com>
2026-03-31 16:19:53 +0530

    code updated 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
-
 import React from "react";
-import './../styles/App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import "./App.css";
+import Userlist from "./Userlist";
+import Userdetails from "./Userdetails";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<Userlist />} />
+          <Route path="/users/:id" element={<Userdetails />} />
+        </Routes>
+      </Router>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
